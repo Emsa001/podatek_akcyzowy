ca66d40e84507177778a68bf5cd975a9ea794168
Emsa001 <emikscur123@gmail.com>
2023-06-19 14:52:06 +0200

    calculator v1

--- src/components/react-form.jsx
+++ src/components/react-form.jsx
@@ -1,47 +1,110 @@
 import { useEffect, useState } from "preact/hooks";
-import { options, currencies } from "@utils/tax_data";
+import { vehicleOptions, currencies, options } from "@utils/tax_data";
 import { getCurrenciesRates } from "@utils/api";
 
 const defaultCurrency = "PLN";
 
 function Form() {
   const [driveType, setDriveType] = useState("");
-  const [lpg, setLpg] = useState(false);
-  const [individualRegister, setIndividualRegister] = useState(false);
-  const [collision, setCollision] = useState(false);
 
   const [carPrice, setCarPrice] = useState(0);
-  const [totalCost, setTotalCost] = useState(0);
-  const [totalTax, setTotalTax] = useState(0);
-  const [currencyValue, setCurrencyValue] = useState(1);
+  const [carPriceConverted, setCarPriceConverted] = useState(0);
+  const [currencyValue, setCurrencyValue] = useState({ code: "PLN", mid: 1 });
   const [currenciesRates, setCurrenciesRates] = useState([]);
+
+  const [totalCost, setTotalCost] = useState(0);
+  const [optionalTax, setOptionalTax] = useState([]);
+  const [totalTax, setTotalTax] = useState(0);
+  const [excise, setExcise] = useState(0);
+  const [excisePercentage, setExcisePercentage] = useState(vehicleOptions[0].value / 100);
+
+  const [engineType, setEngineType] = useState("");
+  const [filteredVehicleOptions, setFilteredVehicleOptions] = useState(vehicleOptions);
+
+  const handleEngineChange = (selectedEngineType) => {
+    setEngineType(selectedEngineType);
+
+    // Filtruj opcje pojazdów na podstawie wybranego rodzaju napędu
+    const filteredOptions = vehicleOptions.filter((option) => {
+      return option.fuelType.includes(selectedEngineType);
+    });
+
+    setFilteredVehicleOptions(filteredOptions);
+    setExcisePercentage(filteredOptions[0].value / 100);
+  };
+
+  const handleCheckboxChange = (tax) => {
+    if (optionalTax.includes(tax)) {
+      // Remove the option if it's already in the array
+      setOptionalTax((prev) => prev.filter((item) => item !== tax));
+    } else {
+      // Add the option if it's not in the array
+      setOptionalTax((prev) => [...prev, tax]);
+    }
+  };
+
+  const handleTaxUpdate = () => {
+    let excise_temp = parseFloat(excisePercentage * carPrice);
+
+    let totalTax_temp = excise_temp;
+    optionalTax.forEach((item) => {
+      totalTax_temp += item;
+    });
+
+    let totalCost_temp = totalTax_temp + parseFloat(carPrice);
+    setExcise(excise_temp.toFixed(2));
+    setTotalCost(parseFloat(totalCost_temp).toFixed(2));
+    setTotalTax(parseFloat(totalTax_temp).toFixed(2));
+  };
 
   const handlePriceChange = (event) => {
     const { value } = event.target;
     if (!isNaN(value)) {
-      return setCarPrice(value);
+      setCarPrice(value);
+      setCarPriceConverted((value * currencyValue.mid).toFixed(2));
     }
   };
 
   const handleCurrencyChange = (event) => {
-    const selectedCurrency = currencies.find((item) => item.code === event.target.value).mid;
-    console.log(selectedCurrency);
+    const currency = event.target.value;
+    let selectedCurrency;
+
+    if (currency === "PLN") {
+      selectedCurrency = { code: "PLN", mid: 1 };
+    } else {
+      selectedCurrency = currenciesRates.find((item) => item.code === currency);
+    }
+
+    setCarPriceConverted((carPrice * selectedCurrency.mid).toFixed(2));
     setCurrencyValue(selectedCurrency);
   };
+
+  useEffect(() => {
+    async function fetchCurrenciesRates() {
+      const data = await getCurrenciesRates("A");
+      setCurrenciesRates(data);
+    }
+    fetchCurrenciesRates();
+    handleEngineChange("benzyna");
+  }, []);
+
+  useEffect(() => {
+    handleTaxUpdate();
+  }, [carPrice, excisePercentage, optionalTax]);
 
   return (
     <div className="container mx-auto py-8">
       <form>
-        <div className="flex flex-wrap -mx-2">
-          <div className="w-full md:w-1/2 px-2 mb-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="priceInput">
               Wartość Pojazdu:
             </label>
-            <div className="relative">
+            <div className="relative flex items-center">
               <input className="appearance-none border rounded-l w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="priceInput" type="text" value={carPrice} onChange={handlePriceChange} required />
-              <select className="absolute inset-y-0 right-0 border border-l-0 rounded-r bg-white text-gray-700 font-bold py-2 px-3 focus:outline-none cursor-pointer" id="currencyInput" name="currencyInput" value={currencyValue} onChange={handleCurrencyChange}>
-                {currencies.map((item) => (
-                  <option key={item} value={item}>
+              <select className="absolute inset-y-0 right-0 border border-l-0 rounded-r bg-white text-gray-700 font-bold py-2 px-3 focus:outline-none cursor-pointer" id="currencyInput" name="currencyInput" value={currencyValue.code} onChange={handleCurrencyChange}>
+                {currencies.map((item, index) => (
+                  <option key={index} value={item} selected={index === 0}>
                     {item}
                   </option>
                 ))}
@@ -49,11 +112,11 @@ function Form() {
             </div>
           </div>
 
-          <div className="w-full md:w-1/2 px-2 mb-4">
+          <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="engineInput">
               Rodzaj Napędu:
             </label>
-            <select className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="engineInput" name="engineInput">
+            <select className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="engineInput" name="engineInput" onChange={(event) => handleEngineChange(event.target.value)}>
               <option value="benzyna">Benzyna</option>
               <option value="diesel">Diesel</option>
               <option value="hybryda">Hybryda</option>
@@ -61,35 +124,35 @@ function Form() {
             </select>
           </div>
 
-          <div className="w-full md:w-1/2 px-2 mb-4">
+          <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="typeInput">
               Rodzaj Pojazdu:
             </label>
-            <select className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="typeInput" name="typeInput">
-              {options.map((item) => (
-                <option key={item.value} value={item.value} data-description={item.description}>
+            <select className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="typeInput" onChange={(event) => setExcisePercentage(event.target.value / 100)}>
+              {filteredVehicleOptions.map((item, index) => (
+                <option key={index} value={item.value} data-description={item.description}>
                   {item.name}
                 </option>
               ))}
             </select>
           </div>
 
-          <div className="w-full px-2 mb-4">
+          <div className="col-span-2">
             <label className="block text-gray-700 text-sm font-bold mb-2">Dodatkowe Parametry:</label>
             <div className="flex items-center mb-2">
-              <input className="mr-2 leading-tight" type="checkbox" id="lpgCheckbox" name="lpgCheckbox" />
+              <input className="mr-2 leading-tight" type="checkbox" id="lpgCheckbox" name="lpgCheckbox" onChange={() => handleCheckboxChange(options.badaniaTechniczne)} autoComplete="off" />
               <label className="text-gray-700 text-sm" htmlFor="lpgCheckbox">
                 Instalacja LPG/CNG
               </label>
             </div>
             <div className="flex items-center mb-2">
-              <input className="mr-2 leading-tight" type="checkbox" id="platesCheckbox" name="platesCheckbox" />
+              <input className="mr-2 leading-tight" type="checkbox" id="platesCheckbox" name="platesCheckbox" onChange={() => handleCheckboxChange(options.tabliceRejestracyjne)} autoComplete="off" />
               <label className="text-gray-700 text-sm" htmlFor="platesCheckbox">
                 Indywidualne Tablice Rejestracyjne
               </label>
             </div>
             <div className="flex items-center">
-              <input className="mr-2 leading-tight" type="checkbox" id="accidentCheckbox" name="accidentCheckbox" />
+              <input className="mr-2 leading-tight" type="checkbox" id="accidentCheckbox" name="accidentCheckbox" onChange={() => handleCheckboxChange(options.badaniaTechniczneKolizja)} autoComplete="off" />
               <label className="text-gray-700 text-sm" htmlFor="accidentCheckbox">
                 Pojazd Powypadkowy/Kolizyjny
               </label>
@@ -97,26 +160,26 @@ function Form() {
           </div>
         </div>
 
-        <div className="flex items-center justify-center">
-          <div className="relative">
+        <div className="flex items-center justify-center mt-8 w-9/12 mx-auto">
+          <div className="relative w-full">
             <div className="my-5 rounded-lg overflow-hidden">
-              <div className="flex flex-col w-full bg-secondary p-4 text-center text-lg text-primary" style={{ backgroundColor: "#d3eaf2" }}>
-                <h4>Całkowity koszt</h4>
-                <span id="totalCost" className="font-semibold text-xl">
+              <div className="flex flex-col w-full bg-blue-200 p-4 text-center text-lg text-primary">
+                <h4 className="mb-2">Całkowity koszt</h4>
+                <span id="totalCost" className="font-semibold text-2xl">
                   {totalCost}
                   {` ${defaultCurrency}`}
                 </span>
               </div>
               <div className="text-center bg-gray-100 w-full grid grid-cols-2 text-lg">
                 <div className="p-4">
-                  <h4>Pojazd</h4>
+                  <h4 className="mb-2">Pojazd</h4>
                   <span id="carCost" className="font-semibold">
-                    {carPrice || 0}
+                    {carPriceConverted || 0}
                     {` ${defaultCurrency}`}
                   </span>
                 </div>
                 <div className="p-4 bg-gray-200">
-                  <h4>Opłaty</h4>
+                  <h4 className="mb-2">Opłaty</h4>
                   <span id="taxes" className="font-semibold">
                     {totalTax}
                     {` ${defaultCurrency}`}
@@ -124,67 +187,34 @@ function Form() {
                 </div>
               </div>
             </div>
-            <table className="mb-0">
+            <table className="w-full mb-0">
               <thead>
                 <tr>
-                  <th className="bg-white">Usługa</th>
-                  <th className="text-right bg-white">Cena</th>
+                  <th className="bg-white py-2 px-4">Usługa</th>
+                  <th className="text-right bg-white py-2 px-4">Cena</th>
                 </tr>
               </thead>
               <tbody className="text-sm">
-                <tr className="bg-blue-50" style={{ backgroundColor: "#e9f5f9" }}>
-                  <td>Cena auta</td>
-                  <td id="carPrice" className="text-right">
+                <tr className="bg-blue-50">
+                  <td className="py-2 px-4">Cena auta</td>
+                  <td id="carPrice" className="text-right py-2 px-4">
                     {carPrice || 0}
                     {` ${defaultCurrency}`}
                   </td>
                 </tr>
                 <tr>
-                  <td>Akcyza</td>
-                  <td className="text-right">0</td>
+                  <td className="py-2 px-4">Akcyza</td>
+                  <td className="text-right py-2 px-4">{excise}</td>
                 </tr>
-                <tr className="bg-blue-50" style={{ backgroundColor: "#e9f5f9" }}>
-                  <td>Badanie techniczne</td>
-                  <td className="text-right">0</td>
-                </tr>
-                <tr>
-                  <td>Tłumaczenia dokumentów</td>
-                  <td className="text-right">0</td>
-                </tr>
-                <tr className="bg-blue-50" style={{ backgroundColor: "#e9f5f9" }}>
-                  <td>Rejestracja pojazdu</td>
-                  <td className="text-right">0</td>
-                </tr>
-                <tr>
-                  <td colSpan="2" className="p-0 bg-blue-50 text-sm">
-                    <table className="border-none m-0">
-                      <tbody className="bg-gray-50-">
-                        <tr className="border-none">
-                          <td> - Tablice rejestracyjne</td>
-                          <td className="text-right"> 0</td>
-                        </tr>
-                        <tr className="border-none">
-                          <td> - Dowód rejestracyjny</td>
-                          <td className="text-right">0</td>
-                        </tr>
-                        <tr className="border-none">
-                          <td> - Pozwolenie czasowe</td>
-                          <td className="text-right">0</td>
-                        </tr>
-                        <tr className="border-none">
-                          <td> - Opłata ewidencyjna</td>
-                          <td className="text-right">0</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </td>
-                </tr>
-                <tr style={{ backgroundColor: "#e9f5f9" }}>
-                  <td>
+                <tr className="bg-blue-50">
+                  <td className="py-2 px-4">
                     <strong>Suma</strong>
                   </td>
-                  <td className="text-right">
-                    <strong>0</strong>
+                  <td className="text-right py-2 px-4">
+                    <strong>
+                      {totalCost}
+                      {` ${defaultCurrency}`}
+                    </strong>
                   </td>
                 </tr>
               </tbody>
--- src/utils/tax_data.js
+++ src/utils/tax_data.js
@@ -1,49 +1,69 @@
-export const options = [
+export const vehicleOptions = [
   {
     name: "Auto osobowe (do 2000 ccm)",
     value: 3.1,
     description: "Podatek akcyzowy dla samochodów osobowych o pojemności do 2000 cm³.",
+    fuelType: ["benzyna", "diesel"],
   },
   {
     name: "Auto osobowe (powyżej 2000 ccm)",
     value: 18.6,
     description: "Podatek akcyzowy dla samochodów osobowych o pojemności powyżej 2000 cm³.",
+    fuelType: ["benzyna", "diesel"],
   },
   {
     name: "Klasyczne hybrydy (HEV) i miękkie hybrydy (MHEV) (powyżej 2000 ccm do 3500 ccm)",
     value: 9.3,
     description: "Podatek akcyzowy dla klasycznych hybryd (HEV) i miękkich hybryd (MHEV) z silnikiem spalinowym o pojemności powyżej 2000 cm³ do 3500 cm³.",
+    fuelType: ["benzyna", "diesel", "hybryda"],
   },
   {
     name: "Hybrydy plug-in (PHEV) (powyżej 2000 ccm do 3500 ccm)",
     value: 9.3,
     description: "Podatek akcyzowy dla hybryd plug-in (PHEV) z silnikiem spalinowym o pojemności powyżej 2000 cm³ do 3500 cm³.",
+    fuelType: ["benzyna", "diesel", "hybryda"],
   },
   {
     name: "Klasyczne hybrydy (HEV) i miękkie hybrydy (MHEV) (do 2000 ccm)",
     value: 1.55,
     description: "Podatek akcyzowy dla klasycznych hybryd (HEV) i miękkich hybryd (MHEV) z silnikiem spalinowym o pojemności do 2000 cm³.",
+    fuelType: ["benzyna", "diesel", "hybryda"],
   },
   {
     name: "Samochody hybrydowe plug-in (PHEV) z silnikami poniżej 2000 ccm",
     value: 0,
     description: "Samochody hybrydowe plug-in (PHEV) z silnikami o pojemności poniżej 2000 cm³ są zwolnione z podatku akcyzowego.",
+    fuelType: ["benzyna", "diesel", "hybryda", "elektryczny"],
   },
   {
     name: "Samochody elektryczne o zwiększonym zasięgu (REX)",
     value: 0,
     description: "Samochody elektryczne o zwiększonym zasięgu (REX) są zwolnione z podatku akcyzowego. Jednakże, ulga ta może być cofnięta w przyszłości.",
+    fuelType: ["elektryczny"],
   },
   {
     name: "Samochody ciężarowe o dopuszczalnej masie całkowitej poniżej 3,5 tony",
     value: 0,
     description: "Samochody ciężarowe o dopuszczalnej masie całkowitej poniżej 3,5 tony nie podlegają opłacie akcyzowej.",
+    fuelType: ["benzyna", "diesel", "hybryda", "elektryczny"],
   },
   {
     name: "Samochody ciężarowe o dopuszczalnej masie całkowitej powyżej 3,5 tony",
     value: 0,
     description: "Samochody ciężarowe o dopuszczalnej masie całkowitej powyżej 3,5 tony nie podlegają opłacie akcyzowej.",
+    fuelType: ["benzyna", "diesel", "hybryda", "elektryczny"],
   },
 ];
 
 export const currencies = ["PLN", "EUR", "GBP", "CHF", "USD"];
+
+export const options = {
+  badaniaTechniczne: 64,
+  badaniaTechniczneKolizja: 94,
+  tlumaczeniaDokumentow: 250,
+  rejestracjaPojazdu: 161.5,
+  tabliceRejestracyjne: 80,
+  dowodRejestracyjny: 54,
+  pozwolenieCzasowe: 26,
+  oplataEwidencyjna: 1.5,
+};
